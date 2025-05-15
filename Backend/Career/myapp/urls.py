39a949b02811e85746afd21ae1d4a114c6b268d8
Muhammad Ahsan <mahsan0516@gmail.com>
2025-05-16 01:15:41 +0500

    commendted a path in urls.py

--- Backend/Career/myapp/urls.py
+++ Backend/Career/myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('users/', views.create_user),
     path('login/', views.get_user),
-    path('recommendations/<str:email>/', views.get_recommendations),
+    # path('recommendations/<str:email>/', views.get_recommendations),
     path('recommendations/', views.get_recommendations),
     path('chatbot/', views.chatbot),
 ]
